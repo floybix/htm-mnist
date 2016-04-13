6c3d049ad628b8bdb89be14cdd58efad6aa219fe
Felix Andrews <felix@nfrac.org>
2016-04-13 10:21:21 +0800

    image perturbations - affine transformations

--- src/clojure/htm_mnist/images.clj
+++ src/clojure/htm_mnist/images.clj
@@ -3,7 +3,8 @@
             [hiccup.core :refer [html]])
   (:import (javax.imageio ImageIO)
            (java.awt.image BufferedImage RenderedImage)
-           (java.awt Image Graphics Color)
+           (java.awt Image Graphics Color RenderingHints)
+           (java.awt.geom AffineTransform)
            (java.io ByteArrayOutputStream File)))
 
 (defn inline-img-tag
@@ -12,7 +13,7 @@
     (ImageIO/write img "png" imgos)
     (let [imgbytes (.toByteArray imgos)
           data-url (str "data:image/png;base64,"
-                        (String. (base64/encode imgbytes)))]
+                        (String. (bytes (base64/encode imgbytes))))]
       (html [:img (assoc attrs :src data-url)]))))
 
 (defn buffered-image
@@ -23,14 +24,6 @@
     (.drawImage g img 0 0 nil)
     (.dispose g)
     bimg))
-
-(defn scale-buffered
-  [^BufferedImage bimg scale]
-  (let [w (.getWidth bimg nil)
-        h (.getHeight bimg nil)]
-    (-> bimg
-        (.getScaledInstance (* w scale) (* h scale) BufferedImage/SCALE_SMOOTH)
-        (buffered-image))))
 
 (defn bytes->image
   [bytes [w h] pad]
@@ -59,3 +52,46 @@
           :let [y (+ y' pad)
                 x (+ x' pad)]]
       (.getRed (Color. (.getRGB img x y))))))
+
+;;; transformations
+
+(defn scale-buffered
+  [^BufferedImage bimg scale]
+  (let [w (.getWidth bimg nil)
+        h (.getHeight bimg nil)]
+    (-> bimg
+        (.getScaledInstance (* w scale) (* h scale) BufferedImage/SCALE_SMOOTH)
+        (buffered-image))))
+
+(defn transform-image
+  [^BufferedImage img xform]
+  (let [out-img (BufferedImage. (.getWidth img nil) (.getHeight img nil)
+                                BufferedImage/TYPE_INT_RGB)
+        g (.createGraphics out-img)
+        hw (quot (.getWidth img) 2)
+        hh (quot (.getHeight img) 2)]
+    (-> g (.getRenderingHints) (.put RenderingHints/KEY_RENDERING
+                                     RenderingHints/VALUE_RENDER_QUALITY))
+    ;; center the transform, for rotations
+    (.preConcatenate xform (AffineTransform/getTranslateInstance hw hh))
+    (.concatenate xform (AffineTransform/getTranslateInstance (- hw) (- hh)))
+    (.drawRenderedImage g img xform)
+    (.dispose g)
+    out-img)
+  )
+
+(defn affine
+  [{:keys [rotate
+           translate
+           scale
+           shear]}]
+  (let [angle rotate
+        [dx dy] translate
+        [shx shy] shear
+        xform (AffineTransform.)]
+    (cond
+      rotate (.rotate xform angle)
+      scale (.scale xform scale scale)
+      translate (.translate xform dx dy)
+      shear (.shear xform shx shy))
+    xform))
